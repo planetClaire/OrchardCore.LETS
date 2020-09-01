8618584a50fcf940cd3edabc818165d2999ee8fe
marshy <marshy@zeta.com.au>
2020-09-01 15:17:01 +0800

    Add a test project

---
+++ Tests/Services/LETSServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace LETS.Tests.Services
+{
+    public class LETSServiceTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
