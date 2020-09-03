42f498c1904c87af538eb499dcb0ac11b6a5d82b
marshy <marshy@zeta.com.au>
2020-09-03 15:24:14 +0800

    version numbers

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "LETS",
     Author = "Claire Botman",
     Website = "https://zeta.com.au",
-    Version = "0.0.1",
+    Version = "0.1.0",
     Description = "LETS Accounting system",
     Category = "Community"
 )]
